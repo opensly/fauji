762a0888a443f8c56f82fd1d1a44f345a5c8abeb
Lakshmikanth Vallampati <kanth.vallampati@gmail.com>
2025-07-22 17:35:39 +0530

    Improved error messages

--- src/run-assertions/test-discovery.js
+++ src/run-assertions/test-discovery.js
@@ -1,18 +1,21 @@
 import isEmptyDir from 'is-empty-dir';
 import { globSync } from 'glob';
+import * as colors from 'colors/safe';
 
 export async function findTestFiles({ targetDir, testMatch }) {
   // Validate the targetDir is legitimate or not
   const empty = await isEmptyDir(targetDir, { ignore: [/^\./, 'node_modules'] });
   if (empty) {
-    console.log(colors.bold(`\n Error: `) + `No test files found in directory: ${targetDir}`);
+    console.log(colors.red(`\n Error: No test files found in directory: ${targetDir}`));
+    process.exit(1);
   }
   let files = [];
   for (const pattern of testMatch) {
     files = files.concat(globSync(pattern, { cwd: targetDir, absolute: true, ignore: ['**/node_modules/**'] }));
   }
   if (files.length === 0) {
-    throw new Error(`No test files matching the allowed patterns were found in targetDir: ${targetDir}`);
+    console.log(colors.red(`\n Error: No test files matching the allowed patterns were found in targetDir: ${targetDir}`));
+    process.exit(1);
   }
   return files;
 }
